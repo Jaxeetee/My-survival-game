05bbff5174766bf5e631229ccd38564fe234b7ba
Jaxeetee <jacob.esclamado19@gmail.com>
2023-04-17 19:45:32 +0800

    added comment in InputManager to plan out the idea

--- Assets/_Project/Script/Input/InputManager.cs
+++ Assets/_Project/Script/Input/InputManager.cs
@@ -6,7 +6,10 @@ public class InputManager : Singleton<InputManager>
     [SerializeField]
     private InputActionAsset _inputActionAsset;
 
-
+    //Create a system wherein I can do the ff:
+    //Enable and Disable Action Maps
+    //Invert controls
 
     
+
 }
